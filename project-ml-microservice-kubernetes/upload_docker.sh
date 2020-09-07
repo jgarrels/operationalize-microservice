5b326eb57e3050104a6eb3baf31c0ce6deb05d14
jgarrels <jonathan.garrels@outlook.com>
2020-09-07 22:13:31 +0000

    Complete script for uploading image to Docker Hub (upload_docker.sh)

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="jgarrels/udacity-operationalize-microservices"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
